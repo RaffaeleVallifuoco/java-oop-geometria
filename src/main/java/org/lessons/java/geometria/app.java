992221e405045e71b63a005f91cb8fb93f006fa2
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2024-05-02 13:03:17 +0200

    EDIT : spacing

--- src/main/java/org/lessons/java/geometria/app.java
+++ src/main/java/org/lessons/java/geometria/app.java
@@ -3,24 +3,37 @@ import java.util.Scanner;
 
 public class app {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+
         System.out.println("");
         System.out.println("- Welcome to myJava geometry tool -");
         System.out.println("--------------------------------------------------------------");
+
         System.out.println("Inseriamo le dimensioni del rettangolo :");
+
         System.out.print("   base : ");
         int base = scan.nextInt();
+
         System.out.print("   Altezza : ");
         int altezza = scan.nextInt();
+
         System.out.println("");
+
         Rettangolo myRettangolo = new Rettangolo(base, altezza) ;
+
         myRettangolo.disegnaRettangolo();
         System.out.println("");
+
         myRettangolo.calcolaPerimetro ();
+
         myRettangolo.calcolaArea ();
+
         System.out.println("--------------------------------------------------------------");
         System.out.println("- end -");
         System.out.println("");
+
+        scan.close();
 
     }
 
